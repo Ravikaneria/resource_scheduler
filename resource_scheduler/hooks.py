f79e76e0710f533a8702c4df0f8d81e6001fc641
Ravikaneria <109785698+Ravikaneria@users.noreply.github.com>
2026-07-07 23:22:22 +0530

    Update hooks.py

--- resource_scheduler/hooks.py
+++ resource_scheduler/hooks.py
@@ -1,1 +1,11 @@
+app_name = "resource_scheduler"
+app_title = "Resource Scheduler"
+app_publisher = "Operations"
+app_description = "Visual Resource Allocation Matrix Dashboard for Engineers"
+app_icon = "octicon octicon-calendar"
+app_color = "blue"
+app_license = "mit"
 
+has_permission = {
+    "Resource Allocation Block": "frappe.permissions.has_permission"
+}
