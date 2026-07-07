eaada1e91dbd1840756aa3de0e438d3c0e6241e3
Ravikaneria <109785698+Ravikaneria@users.noreply.github.com>
2026-07-07 23:21:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="resource_scheduler",
+    version="0.0.1",
+    description="Visual Resource Allocation Matrix Dashboard for Engineers",
+    author="Operations",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
